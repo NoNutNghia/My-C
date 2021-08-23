635c950afbab22bde7bdc167eaaa0953bafd62ea
NoNutNghia <nghia.nn260701@gmail.com>
2021-08-23 16:06:41 +0700

    Fix bug

--- Addition2numbers.c
+++ Addition2numbers.c
@@ -2,34 +2,43 @@
 My programing maybe fix it */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
+void addition(int *, int, int, int[], int[]);
+
+int main(){
     int number1, number2;
+    int i;  
     printf("Enter the number of digits 1: ");
     scanf(" %d", &number1);
     printf("Enter the number of digits 2: ");
     scanf(" %d", &number2);
-    int i;
     int num_array1[number1];
     int num_array2[number2];
-    int *sum_array = (int *) malloc(1 * sizeof(int));
-    if(number1 > number2) sum_array = realloc(sum_array, number1 * sizeof(int));
-    else sum_array = realloc(sum_array, number2 * sizeof(int));
     printf("Enter the first number 1: ");
     for(i = 0; i < number1; i++){
         scanf(" %d", &num_array1[i]);
     }
-    printf("Enter the first number 2: ");
+    printf("Enter the second number 2: ");
     for(i = 0; i < number2; i++){
         scanf(" %d", &num_array2[i]);
     }
+    int *sum_array; 
+    sum_array = (int *) malloc(1 * sizeof(int));
+    if(number1 >= number2) addition(sum_array, number1, number2, num_array1, num_array2);
+    else addition(sum_array, number2, number1, num_array2, num_array1 );
+    free(sum_array);
+    return 0;
+}
+
+void addition(int *sum_array, int number1, int number2, int *num_arr1, int *num_arr2){  
+    sum_array = (int *) realloc(sum_array, number1 * sizeof(int)); 
     int delta;
-    if(number1 > number2) delta = number1 - number2;
-    else delta = number2 - number1;
     int remider = 0, temp;
-    for(i = number1 - 1; i >= 0; i--){
-        temp = num_array1[i] + num_array2[i] + remider;
+    int i; 
+    delta = number1 - number2;
+    for(i = number2 - 1; i >= 0; i--){
+        temp = num_arr2[i] + num_arr1[i + delta] + remider;
         if(temp >= 10){ 
             temp -= 10;
             remider = 1;
@@ -39,14 +48,12 @@ int main()
             remider = 0;
         }
     }
-    *(sum_array + delta) = num_array2[delta] + remider;
+    *(sum_array + delta) = num_arr1[delta] + remider;
     for(i = delta - 1; i >= 0; i++){
-        *(sum_array + i) = num_array2[i];
+        *(sum_array + i) = num_arr1[i];
     }
     printf("Result: \n");
-    for(i = 0; i < number2; i++){
+    for(i = 0; i < number1; i++){
         printf(" %d ", *(sum_array + i));
     }
-    free(sum_array);
-    return 0;
 }
